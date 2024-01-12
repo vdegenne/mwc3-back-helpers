03782cd9abb1e26c8c2802ab53d3ee183713a660
vdegenne <vdegenne@gmail.com>
2024-01-12 20:55:06 +0100

    elements imports map generative script

---
+++ scripts/generate-imports-map.ts
@@ -0,0 +1,41 @@
+import {existsSync} from 'node:fs';
+import {join} from 'node:path';
+import {readFile, writeFile} from 'node:fs/promises';
+import {findImportsInContent, type ImportsMap} from '../src/imports.ts';
+
+const allFilePath = join('node_modules', '@material', 'web', 'all.js');
+
+async function main() {
+	if (!existsSync(allFilePath)) {
+		throw new Error(
+			"'@material/web' doesn't seem to be installed, please run `npm i` before building."
+		);
+	}
+	const allFileContent = await readFile(allFilePath);
+
+	// Process the elements imports Map
+	const imports = findImportsInContent(allFileContent.toString());
+	const importsMap: ImportsMap = {};
+
+	const elementNameRegex = /\/([^/.]+)\.js/;
+	for (const importee of imports) {
+		const elementNameMatch = importee.match(elementNameRegex)!;
+		importsMap[`md-${elementNameMatch[1]}`] = importee.replace(
+			/^\./,
+			'@material/web'
+		);
+	}
+
+	await writeFile(
+		'src/md-elements-imports-map.ts',
+		`import {type ImportsMap} from './imports.js';
+
+export const MdElementsImportsMap: ImportsMap = ${JSON.stringify(
+			importsMap,
+			null,
+			2
+		)};`
+	);
+}
+
+await main();
