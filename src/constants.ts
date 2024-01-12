8a2885f288ca4e1f007368912dca315336f39659
vdegenne <vdegenne@gmail.com>
2024-01-12 22:21:39 +0100

    few regexs

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const MATERIAL_ALL_IMPORT_REGEX =
+	/import\s+['"]@material\/web\/all(\.js)?['"]\s*( ?;)?/;
+
+export const MD_ELEMENT_REGEX = /<(md-[\w-]+)/;
+export const MD_ICON_REGEX = /(<md-icon[^>]*>)\s*([a-z_]+)\s*(<\/md-icon\s*>)/g;
