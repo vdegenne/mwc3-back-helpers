27b02c2d67ef90af3274e426c49aa3ebe9534d31
vdegenne <vdegenne@gmail.com>
2024-01-13 13:40:02 +0100

    findElementsFromContent tests

--- src/md-elements.ts
+++ src/md-elements.ts
@@ -1,12 +1,28 @@
 import {MdElementsImportsMap} from './md-elements-imports-map.js';
+import {MD_ELEMENT_REGEX, matchAllFromContent} from './regexps.js';
 
-export {MdElementsImportsMap};
+export {MdElementsImportsMap, MD_ELEMENT_REGEX};
 
 /**
  * Take a list of element names and return a filtered array
  * keeping only md-* elements that exist in the library.
  */
-export async function pruneFakeElements(elements: string[]) {
+export function pruneFakeElements(elements: string[]) {
 	const availableElements = Object.keys(MdElementsImportsMap);
 	return elements.filter((el) => availableElements.includes(el));
+}
+
+export function findElementsFromContent(content: string, stripComments = true) {
+	const elementsSet = new Set<string>();
+
+	const matches = matchAllFromContent(content, MD_ELEMENT_REGEX, stripComments);
+
+	for (const match of matches) {
+		elementsSet.add(match[1]);
+	}
+
+	let elements = Array.from(elementsSet);
+	elements = pruneFakeElements(elements);
+
+	return elements;
 }
--- src/test/md-elements_test.ts
+++ src/test/md-elements_test.ts
@@ -1,5 +1,9 @@
 import {expect} from 'chai';
-import {MdElementsImportsMap, pruneFakeElements} from '../md-elements.js';
+import {
+	MdElementsImportsMap,
+	findElementsFromContent,
+	pruneFakeElements,
+} from '../md-elements.js';
 
 describe('md-elements module', () => {
 	describe('imports map', () => {
@@ -14,9 +18,81 @@ describe('md-elements module', () => {
 		});
 	});
 
-	it('can prune fake elements', async () => {
+	it('can prune fake elements', () => {
 		const elements = ['md-icon', 'md-fake-element', 'md-icon-button', 'fake'];
-		const pruned = await pruneFakeElements(elements);
+		const pruned = pruneFakeElements(elements);
 		expect(pruned).to.deep.equal(['md-icon', 'md-icon-button']);
+	});
+
+	describe('findElementsFromContent', () => {
+		it('should find elements', () => {
+			const input = `<md-icon>settings</md-icon>`;
+			const elements = findElementsFromContent(input);
+
+			expect(elements.length).to.equal(1);
+			expect(elements[0]).to.equal('md-icon');
+		});
+
+		it('should find multiple elements', () => {
+			const input = `
+          <md-icon>settings</md-icon>
+          <md-menu>
+            <md-menu-item>test</md-menu-item>
+          </md-menu>
+          `;
+			const elements = findElementsFromContent(input);
+
+			expect(elements).to.deep.equal(['md-icon', 'md-menu', 'md-menu-item']);
+		});
+
+		it('should return a distinct result', () => {
+			const input = `
+          <md-icon>settings</md-icon>
+          <md-icon>delete</md-icon>
+          `;
+			const elements = findElementsFromContent(input);
+
+			expect(elements.length).to.equal(1);
+			expect(elements[0]).to.equal('md-icon');
+		});
+
+		it('should ignore comments by default', () => {
+			const input = ` // <md-fab></md-fab>
+          // <md-icon>settings</md-icon>
+          <!-- <md-chip-set></md-chip-set> -->
+          <md-circular-progress></md-circular-progress>
+          `;
+			const elements = findElementsFromContent(input);
+
+			expect(elements.length).to.equal(1);
+			expect(elements[0]).to.equal('md-circular-progress');
+		});
+
+		it('can take an argument to include comments', () => {
+			const input = ` // <md-fab></md-fab>
+          // <md-icon>settings</md-icon>
+          <!-- <md-chip-set></md-chip-set> -->
+          <md-circular-progress></md-circular-progress>
+          `;
+			const elements = findElementsFromContent(input, false);
+
+			expect(elements.length).to.equal(4);
+			expect(elements).to.deep.equal([
+				'md-fab',
+				'md-icon',
+				'md-chip-set',
+				'md-circular-progress',
+			]);
+		});
+
+		it('should prune fake elements', () => {
+			const input = `
+          <md-icon>settings</md-icon>
+          <md-this-element-does-not-exist></md-this-element-does-not-exist>`;
+			const elements = findElementsFromContent(input);
+
+			expect(elements.length).to.equal(1);
+			expect(elements[0]).to.equal('md-icon');
+		});
 	});
 });
